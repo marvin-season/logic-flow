656c5f6f4824f422defa00cc681b88b5d4b6383a
marvin <sea-son@foxmail.com>
2024-08-05 10:31:58 +0800

    feat: style

--- src/pages/workflow-page/nodes/base/node.tsx
+++ src/pages/workflow-page/nodes/base/node.tsx
@@ -1,4 +1,4 @@
-import {cloneElement, FC, memo, ReactElement} from 'react';
+import {FC, memo, ReactElement} from 'react';
 import {NodeProps} from 'reactflow';
 
 
@@ -11,14 +11,16 @@ const BaseNode: FC<BaseNodeProps> = ({id, data, children}) => {
 
   return (
     <div
+      className={'border-primary-600 border-solid flex border-[2px] rounded-2xl'}
       style={{
         width: 'auto',
         height: 'auto',
       }}
     >
-      <div className={'group relative pb-1 shadow-xs border border-transparent rounded-[15px] w-[240px] bg-[#fcfdff] border-solid'}>
+      <div
+        className={'p-6 group relative pb-1 shadow-xs border border-transparent rounded-[15px] w-[240px] bg-[#fcfdff] border-solid'}>
+        {children}
       </div>
-      {cloneElement(children, {id, data})}
 
     </div>
   );
