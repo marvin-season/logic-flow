8420893beb053d5fe009bce6bdfc105fa72fd1af
marvin <sea-son@foxmail.com>
2024-08-06 14:29:53 +0800

    fix: add node type

--- src/pages/workflow-page/handle/index.tsx
+++ src/pages/workflow-page/handle/index.tsx
@@ -1,14 +1,22 @@
 import { memo } from "react";
 import { Handle, Position } from "reactflow";
+import { FlowNode } from "../types";
 
-export const BaseSourceHandle = memo(({ ...props }) => {
+type NodeHandleProps = {
+  handleId: string;
+  handleClassName?: string;
+  nodeSelectorClassName?: string;
+} & Pick<FlowNode, 'id' | 'data'>;
+
+
+export const BaseSourceHandle = memo<NodeHandleProps>(({ ...props }) => {
   return (
     <>
       <Handle type="source" position={Position.Right} id="right" />
     </>
   );
 });
-export const BaseTargetHandle = memo(({ ...props }) => {
+export const BaseTargetHandle = memo<NodeHandleProps>(({ ...props }) => {
   return (
     <>
       <Handle type="target" position={Position.Left} id="left" />
--- src/pages/workflow-page/types.ts
+++ src/pages/workflow-page/types.ts
@@ -1,4 +1,32 @@
+import { Node } from "reactflow";
+
 export enum BlockEnum {
   Start = "start",
   End = "end",
 }
+
+export type CommonNodeType<T = {}> = {
+  _connectedSourceHandleIds?: string[];
+  _connectedTargetHandleIds?: string[];
+  _isSingleRun?: boolean;
+  _isCandidate?: boolean;
+  _isBundled?: boolean;
+  _children?: string[];
+  _isEntering?: boolean;
+  _showAddVariablePopup?: boolean;
+  _holdAddVariablePopup?: boolean;
+  _iterationLength?: number;
+  _iterationIndex?: number;
+  isIterationStart?: boolean;
+  isInIteration?: boolean;
+  iteration_id?: string;
+  selected?: boolean;
+  title: string;
+  desc: string;
+  type: BlockEnum;
+  width?: number;
+  height?: number;
+} & T;
+
+
+export type FlowNode<T = {}> = Node<CommonNodeType<T>>;
