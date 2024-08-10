507d4df6065c6e22809af3e0a08f04c03fa899bb
marvin <sea-son@foxmail.com>
2024-08-10 11:42:45 +0800

    feat: react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './main.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import "./main.css";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import ApplicationPage from "./pages/application/index.tsx";
+import WorkflowPage from "./pages/workflow/index.tsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <div></div>
+        <div>
+          <Outlet />
+        </div>
+      </div>
+    ),
+    children: [
+      {
+        path: "flow",
+        element: (
+          <Suspense fallback={"loading"}>
+            <WorkflowPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "app",
+        element: (
+          <Suspense fallback={"loading"}>
+            <ApplicationPage />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
