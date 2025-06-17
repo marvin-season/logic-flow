6dde0fa921596fc5528067b2ded12832ebfe12cb
Marvin <marvin@mail.pollo.ai>
2025-06-17 10:39:26 +0800

    chore: 删除没用的东西

--- src/main.tsx
+++ src/main.tsx
@@ -1,74 +1,9 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
-import {
-  createBrowserRouter,
-  Outlet,
-  Route,
-  RouterProvider,
-  Routes,
-  useNavigate,
-} from "react-router-dom";
-import ApplicationPage from "./pages/application/index.tsx";
 import WorkflowPage from "./pages/workflow/index.tsx";
-import '@/utils/javascript-node.ts'
-const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: () => {
-      const navigate = useNavigate();
-
-      return (
-        <div>
-          <div>
-            <div
-              onClick={() => {
-                navigate("/flow");
-              }}
-            >
-              flow
-            </div>
-            <div
-              onClick={() => {
-                navigate("/app");
-              }}
-            >
-              app
-            </div>
-          </div>
-          <div>
-            <div>
-              <div id="portal-action"></div>
-            </div>
-            <div>
-              <Outlet />
-            </div>
-          </div>
-        </div>
-      );
-    },
-    children: [
-      {
-        path: "flow",
-        element: (
-          <Suspense fallback={"loading"}>
-            <WorkflowPage />
-          </Suspense>
-        ),
-      },
-      {
-        path: "app",
-        element: (
-          <Suspense fallback={"loading"}>
-            <ApplicationPage />
-          </Suspense>
-        ),
-      },
-    ],
-  },
-]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <WorkflowPage />
   </React.StrictMode>
 );
